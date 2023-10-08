3d664993c796db6b935b5cecb5a91c6ebb56b949
v473r10 <fac.valerio@gmail.com>
2023-10-08 19:58:57 -0300

    Initial commit

---
+++ src/main/kotlin/com/codedicated/Application.kt
@@ -0,0 +1,16 @@
+package com.codedicated
+
+import com.codedicated.plugins.*
+import io.ktor.server.application.*
+
+fun main(args: Array<String>) {
+    io.ktor.server.netty.EngineMain.main(args)
+}
+
+fun Application.module() {
+    configureSecurity()
+    configureMonitoring()
+    configureSerialization()
+    configureSockets()
+    configureRouting()
+}
---
+++ src/main/kotlin/com/codedicated/plugins/Routing.kt
@@ -0,0 +1,13 @@
+package com.codedicated.plugins
+
+import io.ktor.server.application.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+
+fun Application.configureRouting() {
+    routing {
+        get("/") {
+            call.respondText("Hello World!")
+        }
+    }
+}
